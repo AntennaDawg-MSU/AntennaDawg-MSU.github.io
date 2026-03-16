5ad8336680bd4ae91d1aa9947980b531a93769ac
AntennaDawg-MSU <133796375+AntennaDawg-MSU@users.noreply.github.com>
2026-03-16 14:06:52 -0500

    Update guide.js

--- homework3/guide.js
+++ homework3/guide.js
@@ -3,3 +3,11 @@ export const homework3 = {
   "Q2": ["2a", "2b", "2c", "2d", "2e"],
   "Q3": ["3a", "3b", "3c", "3d", "3e"]
 };
+
+export const part = {
+  "I",
+  "II",
+  "III",
+  "IV",
+  "V"
+}
