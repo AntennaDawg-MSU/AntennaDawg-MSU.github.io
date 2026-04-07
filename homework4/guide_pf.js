28f2d27076ca055b52a2d529d0697ebeb0ae69eb
AntennaDawg-MSU <133796375+AntennaDawg-MSU@users.noreply.github.com>
2026-04-07 18:25:51 -0500

    Update prompts and versions in guide_pf.js

--- homework4/guide_pf.js
+++ homework4/guide_pf.js
@@ -6,64 +6,214 @@ window.homeworkGuides[4] = {
   questions: {
     1: {
       I: {
-        prompt: "What is the intersection point (x,y) of the first wavelength on the right side of the prism?",
+        prompt: "What is the intersection point (x,y) of the ray inside the prism with the right side of the prism?",
         inputs: [
           { label: "(X.XXX, Y.YYY) - slight differences may occur :",      type: "text"}
         ],
         versions: {
-          a: ["(0.414, 0.149)"],
-          b: ["(0.531, -0.053)"],
-          c: ["(0.451, 0.085)"],
-          d: ["(0.426, 0.128)"],
-          e: ["(0.450, 0.086)"],
-          f: ["(0.514, -0.024)"]
+          "01-Red": ["(0.447, 0.092)"],
+          "02-Orange": ["(0.451, 0.085)"],
+          "03-Yellow": ["(0.454, 0.080)"],
+          "04-Green": ["(0.459, 0.070)"],
+          "05-Blue": ["(0.468, 0.056)"],
+          "06-Purple": ["(0.476, 0.042)"]
         }
       },
       II: {
-        prompt: "Given that x = 2, what is the y value for the first wavelength for the ray exiting the prism?",
+        prompt: "Given that x = 2, what is the y value of the ray that has exited the prism?",
         inputs: [
           { label: "y=Y.YYY", type: "number", tolerance: 0.05 }
         ],
         versions: {
-          red: [0.608],
-          b: [-0.539],
-          c: [0.132],
-          d: [0.522],
-          e: [0.16],
-          f: [-0.525]
+          "01-Red": [0.302],
+          "02-Orange": [0.250],
+          "03-Yellow": [0.210],
+          "04-Green": [0.124],
+          "05-Blue": [-0.007],
+          "06-Purple": [-0.150]
         }
-      },
-      III: {
-        prompt: "What is the intersection point (x,y) of the second wavelength on the right side of the prism?",
+      }
+    },
+    2: {
+      I: {
+        prompt: "What is the intersection point (x,y) of the ray inside the prism with the right side of the prism?",
         inputs: [
           { label: "(X.XXX, Y.YYY) - slight differences may occur :",      type: "text"}
         ],
         versions: {
-          a: ["(0.421, 0.137)"],
-          b: ["(0.537, -0.065)"],
-          c: ["(0.457, 0.074)"],
-          d: ["(0.429, 0.123)"],
-          e: ["(0.492, 0.015)"],
-          f: ["(0.523, -0.04)"]
+          "01-Red": ["(0.414, 0.149)"],
+          "02-Orange": ["(0.416, 0.145)"],
+          "03-Yellow": ["(0.418, 0.142)"],
+          "04-Green": ["(0.421, 0.137)"],
+          "05-Blue": ["(0.426, 0.129)"],
+          "06-Purple": ["(0.431, 0.120)"]
         }
-        
       },
-      IV: {
-        prompt: "Given that x = 2, what is the y value for the second wavelength for the ray exiting the prism?",
+      II: {
+        prompt: "Given that x = 2, what is the y value of the ray that has exited the prism?",
         inputs: [
-          { label: "y = Y.YYY ", type: "number", tolerance: 0.05 }
+          { label: "y=Y.YYY", type: "number", tolerance: 0.05 }
         ],
         versions: {
-          a: [0.505],
-          b: [-0.702],
-          c: [0.024],
-          d: [0.486],
-          e: [-0.801],
-          f: [-0.784]
+          "01-Red": [0.608],
+          "02-Orange": [0.578],
+          "03-Yellow": [0.554],
+          "04-Green": [0.505],
+          "05-Blue": [0.432],
+          "06-Purple": [0.355]
         }
       }
-    }
-    
+    },
+    3: {
+      I: {
+        prompt: "What is the intersection point (x,y) of the ray inside the prism with the right side of the prism?",
+        inputs: [
+          { label: "(X.XXX, Y.YYY) - slight differences may occur :",      type: "text"}
+        ],
+        versions: {
+          "01-Red": ["(0.528, -0.048)"],
+          "02-Orange": ["(0.530, -0.051)"],
+          "03-Yellow": ["(0.531, -0.053)"],
+          "04-Green": ["(0.533, -0.058)"],
+          "05-Blue": ["(0.537, -0.065)"],
+          "06-Purple": ["(0.541, -0.072)"]
+        }
+      },
+      II: {
+        prompt: "Given that x = 2, what is the y value of the ray that has exited the prism?",
+        inputs: [
+          { label: "y=Y.YYY", type: "number", tolerance: 0.05 }
+        ],
+        versions: {
+          "01-Red": [-0.476],
+          "02-Orange": [-0.510],
+          "03-Yellow": [-0.539],
+          "04-Green": [-0.601],
+          "05-Blue": [-0.702],
+          "06-Purple": [-0.824]
+        }
+      }
+    },
+    4: {
+      I: {
+        prompt: "What is the intersection point (x,y) of the ray inside the prism with the right side of the prism?",
+        inputs: [
+          { label: "(X.XXX, Y.YYY) - slight differences may occur :",      type: "text"}
+        ],
+        versions: {
+          "01-Red": ["(0.451, 0.085)"],
+          "02-Orange": ["(0.455, 0.078)"],
+          "03-Yellow": ["(0.457, 0.074)"],
+          "04-Green": ["(0.463, 0.064)"],
+          "05-Blue": ["(0.471, 0.050)"],
+          "06-Purple": ["(0.479, 0.036)"]
+        }
+      },
+      II: {
+        prompt: "Given that x = 2, what is the y value of the ray that has exited the prism?",
+        inputs: [
+          { label: "y=Y.YYY", type: "number", tolerance: 0.05 }
+        ],
+        versions: {
+          "01-Red": [0.132],
+          "02-Orange": [0.072],
+          "03-Yellow": [0.024],
+          "04-Green": [-0.079],
+          "05-Blue": [-0.243],
+          "06-Purple": [-0.436]
+        }
+      }
+    },
+    5: {
+      I: {
+        prompt: "What is the intersection point (x,y) of the ray inside the prism with the right side of the prism?",
+        inputs: [
+          { label: "(X.XXX, Y.YYY) - slight differences may occur :",      type: "text"}
+        ],
+        versions: {
+          "01-Red": ["(0.422, 0.135)"],
+          "02-Orange": ["(0.423, 0.133)"],
+          "03-Yellow": ["(0.424, 0.131)"],
+          "04-Green": ["(0.426, 0.128)"],
+          "05-Blue": ["(0.429, 0.123)"],
+          "06-Purple": ["(0.432, 0.119)"]
+        }
+      },
+      II: {
+        prompt: "Given that x = 2, what is the y value of the ray that has exited the prism?",
+        inputs: [
+          { label: "y=Y.YYY", type: "number", tolerance: 0.05 }
+        ],
+        versions: {
+          "01-Red": [0.573],
+          "02-Orange": [0.558],
+          "03-Yellow": [0.547],
+          "04-Green": [0.522],
+          "05-Blue": [0.486],
+          "06-Purple": [0.447]
+        }
+      }
+    },
+    6: {
+      I: {
+        prompt: "What is the intersection point (x,y) of the ray inside the prism with the right side of the prism?",
+        inputs: [
+          { label: "(X.XXX, Y.YYY) - slight differences may occur :",      type: "text"}
+        ],
+        versions: {
+          "01-Red": ["(0.444, 0.098)"],
+          "02-Orange": ["(0.450, 0.086)"],
+          "03-Yellow": ["(0.455, 0.077)"],
+          "04-Green": ["(0.465, 0.060)"],
+          "05-Blue": ["(0.479, 0.037)"],
+          "06-Purple": ["(0.492, 0.015)"]
+        }
+      },
+      II: {
+        prompt: "Given that x = 2, what is the y value of the ray that has exited the prism?",
+        inputs: [
+          { label: "y=Y.YYY", type: "number", tolerance: 0.05 }
+        ],
+        versions: {
+          "01-Red": [0.262],
+          "02-Orange": [0.160],
+          "03-Yellow": [0.078],
+          "04-Green": [-0.098],
+          "05-Blue": [-0.390],
+          "06-Purple": [-0.801]
+        }
+      }
+    },
+   7: {
+      I: {
+        prompt: "What is the intersection point (x,y) of the ray inside the prism with the right side of the prism?",
+        inputs: [
+          { label: "(X.XXX, Y.YYY) - slight differences may occur :",      type: "text"}
+        ],
+        versions: {
+          "01-Red": ["(0.510, -0.017)"],
+          "02-Orange": ["(0.512, -0.021)"],
+          "03-Yellow": ["(0.514, -0.024)"],
+          "04-Green": ["(0.518, -0.03)"],
+          "05-Blue": ["(0.523, -0.04)"],
+          "06-Purple": ["(0.528, -0.049)"]
+        }
+      },
+      II: {
+        prompt: "Given that x = 2, what is the y value of the ray that has exited the prism?",
+        inputs: [
+          { label: "y=Y.YYY", type: "number", tolerance: 0.05 }
+        ],
+        versions: {
+          "01-Red": [-0.432],
+          "02-Orange": [-0.483],
+          "03-Yellow": [-0.525],
+          "04-Green": [-0.620],
+          "05-Blue": [-0.784],
+          "06-Purple": [-1.005]
+        }
+      }
+    }  
   }
 };
 
